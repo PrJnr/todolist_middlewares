fe695e79d04f463cf8ccc281e2123530fe257f74
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2021-09-09 21:46:18 -0300

    created Middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find((user)=>user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: "Usuario não existe!"})
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request;
+
+  const todosCount = user.todos.length;
+  console.log(todosCount)
+
+  if(user.pro === false && todosCount < 10){
+    return next();
+   
+  }else if(user.pro){
+    return next();
+  }else{
+    return response.status(403).json({error: "Limite de Tarefas atingido, assine o plano pro!"})
+  }
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+  if (!user) return response.status(404).json({ error: 'user not found' });
+
+  const checkUuidIsValid = validate(id);
+  if (!checkUuidIsValid) return response.status(400).json({ error: 'Id invalid' });
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) return response.status(404).json({ error: 'todo not found' });
+  
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  if (!id) return response.status(400).json({ error: 'id is required' });
+
+  const user = users.find((user) => user.id === id);
+  if (!user) return response.status(404).json({ error: 'user not found' });
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
